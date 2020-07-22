cf9740324611c9b9769a334fff4dc6af00d0007a
Karen Perez <karenp.flores47@gmail.com>
2020-07-21 19:31:35 -0500

    inventory crud complete

---
+++ public/precache-manifest.cd8903824d1de0bda6b4ab322431e61c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51aa6327d30fc1c739a97f13e81d52d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc4217cd175e4b953c57",
+    "url": "/static/css/2.8c4e8b5e.chunk.css"
+  },
+  {
+    "revision": "bf07e3c45b486cc194b6",
+    "url": "/static/css/main.acbea83c.chunk.css"
+  },
+  {
+    "revision": "fc4217cd175e4b953c57",
+    "url": "/static/js/2.a4937fae.chunk.js"
+  },
+  {
+    "revision": "1a210ccb07c1962e47d7db0f05093ffb",
+    "url": "/static/js/2.a4937fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf07e3c45b486cc194b6",
+    "url": "/static/js/main.6b4c3515.chunk.js"
+  },
+  {
+    "revision": "9271685765308b901460",
+    "url": "/static/js/runtime-main.5c8c9a9e.js"
+  }
+]);
